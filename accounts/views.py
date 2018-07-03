fb166360a2c6174066f84effb0e4a3e1d998215f
sherr2018 <sherinv017@gmail.com>
2018-07-03 17:26:39 +0530

    acounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login,logout
 # Create your views here.
 
 def signup_view(request):
-    form = UserCreationForm
-    return render(request,'accounts/signup.html',{'form':form});
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user =form.save()# if form is valid then form will save to database
+            #log in the user in
+            login(request,user)
+
+            return redirect('articles:list')
+        else:
+            return render(request,'accounts/signup.html',{'form':form});
+    else:
+        form = UserCreationForm()
+        return render(request,'accounts/signup.html',{'form':form});
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data = request.POST)#request.POST is not first param on this function thats the reason we need to add name tag'data'
+        if form.is_valid():
+            #login the User
+            user = form.get_user()
+            login(request,user)
+            return redirect('articles:list')
+    else:
+        form = AuthenticationForm()
+    return render(request,'accounts/login.html',{'form':form})
+def logout_view(request):
+    if request.method == POST:
+        logout(request)
+    return redirect('articles:list')
